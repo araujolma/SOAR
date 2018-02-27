57e73d9ed819aac71c2c0374984d92e1a412064c
araujolma <levimaiaaraujo@gmail.com>
2018-02-27 20:44:18 -0300

    Fixing stupid plot mistake in plotSol and compWith
    Only two lines of code were altered, but it prevented the code
    from running!

--- probRock.py
+++ probRock.py
@@ -1226,10 +1226,10 @@ class prob(sgra):
             self.plotCat(acc*1e3,color='y',piIsTime=piIsTime,labl='Accel.')
             if piIsTime:
                 plt.plot([0.0,self.pi.sum()],\
-                          1e3*self.restrictions['acc_max']*numpy.array([1.0,1.0]),mark='--')
+                          1e3*self.restrictions['acc_max']*numpy.array([1.0,1.0]),'--')
             else:
                 plt.plot([0.0,self.s+1],\
-                          1e3*self.restrictions['acc_max']*numpy.array([1.0,1.0]),mark='--')
+                          1e3*self.restrictions['acc_max']*numpy.array([1.0,1.0]),'--')
 
             plt.grid(True)
             if piIsTime:
@@ -1668,7 +1668,7 @@ class prob(sgra):
         altSolAcc = altSol.calcAcc()
         plt.plot([0.0,self.pi.sum()],\
                   1e3*self.restrictions['acc_max']*numpy.array([1.0,1.0]),\
-                  mark='--',label='accel. limit')
+                  '--',label='accel. limit')
         altSol.plotCat(1e3*altSolAcc,labl=altSolLabl)
         self.plotCat(1e3*solAcc,mark='--',color='y',labl=currSolLabl)
         plt.grid(True)
