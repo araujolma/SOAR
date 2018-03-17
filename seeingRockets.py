719e4cef0ce48352561525a44348061e87050dbb
CarlosCHMS <msouza.ch@gmail.com>
2018-03-16 22:39:21 -0300

    Several improvements
    Reordered classes
    Option for mayavi (high quality figures)

--- seeingRockets.py
+++ seeingRockets.py
@@ -7,62 +7,130 @@ Created on Tue Mar  6 19:50:50 2018
 
 @author: carlos
 
+Requirement:
+    mayavi
+
 """
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy
+from mayavi import mlab
 
 
-class rocket():
+#############################################################################
+# Basic objects
 
-    def __init__(self, centralBody, boosterBody, Nbooster, d):
 
-        self.cb = centralBody.copy()
-        self.bb = boosterBody.copy()
-        self.Nb = Nbooster
-        self.d = d
-        self.Lmax = 0.0
+class surfClass():
 
-        self.calculate()
+    def __init__(self, x, y, z, color):
+
+        self.x = x
+        self.y = y
+        self.z = z
+        self.color = color
 
     def copy(self):
 
-        return rocket(self.cb, self.bb, self.Nb, self.d)
+        return surfClass(self.x, self.y, self.z, self.color)
 
-    def calculate(self):
+    def translate(self, delta):
 
-        dt = 2*numpy.pi/self.Nb
-        self.bList = []
-        for ii in range(0, self.Nb):
-            aux = self.bb.copy()
-            aux.translate([self.d, 0.0, 0.0])
+        self.x = self.x + delta[0]
+        self.y = self.y + delta[1]
+        self.z = self.z + delta[2]
+
+    def rotateZ(self, theta):
+
+        x0 = self.x.copy()
+        y0 = self.y.copy()
+        self.x = x0*numpy.cos(theta) + y0*numpy.sin(theta)
+        self.y = y0*numpy.cos(theta) - x0*numpy.sin(theta)
+
+    def rotateY(self, theta):
+
+        x0 = self.z.copy()
+        y0 = self.x.copy()
+        self.z = x0*numpy.cos(theta) + y0*numpy.sin(theta)
+        self.x = y0*numpy.cos(theta) - x0*numpy.sin(theta)
+
+    def plot(self, ax):
+
+        aux = (self.color[0], self.color[1], self.color[2])
+        ax.mesh(self.x, self.y, self.z, color=aux)
+
+        return ax
+
+    def matPlot(self, ax):
+
+        ax.plot_surface(self.x, self.y, self.z, antialiased=False,
+                        color=self.color, linewidth=0)
+
+        return ax
+
+
+class group():
+
+    def __init__(self, objList):
+
+        self.objList = []
+        for obj in objList:
+            self.objList.append(obj.copy())
+
+    def copy(self):
+
+        return group(self.objList)
+
+    def around(self, group2, N, d):
+
+        dt = 2*numpy.pi/N
+        for ii in range(0, N):
+            aux = group2.copy()
+            aux.translate([d, 0.0, 0.0])
             aux.rotateZ(dt*ii)
-            self.bList.append(aux)
-            if self.Lmax < aux.z1:
-                self.Lmax = aux.z1
+            self.objList.append(aux)
 
-        self.bList.append(self.cb)
-        if self.Lmax < self.cb.z1:
-            self.Lmax = self.cb.z1
+    def translate(self, delta):
 
-        	
+        for obj in self.objList:
+            obj.translate(delta)
+
+    def rotateZ(self, theta):
+
+        for obj in self.objList:
+            obj.rotateZ(theta)
+
+    def rotateY(self, theta):
+
+        for obj in self.objList:
+            obj.rotateY(theta)
 
     def plot(self, ax):
 
-        for body in self.bList:
-            body.plot(ax)
+        for obj in self.objList:
+            obj.plot(ax)
 
         return ax
 
+    def matPlot(self, ax):
 
-class body():
+        for obj in self.objList:
+            obj.matPlot(ax)
 
-    def __init__(self, stgList):
+        return ax
 
-        self.stgList = []
-        for stg in stgList:
-            self.stgList.append(stg.copy())
+#############################################################################
+# Ineritated classes
+
+
+class stack(group):
+
+    def __init__(self, objList):
+
+        self.objList = []
+        for obj in objList:
+            self.objList.append(obj.copy())
 
         self.r0 = 0.0
         self.r1 = 0.0
@@ -73,85 +141,21 @@ class body():
 
     def copy(self):
 
-        return body(self.stgList)
+        return stack(self.objList)
 
     def calculate(self):
 
-        self.r0 = self.stgList[0].r0
-        self.z0 = self.stgList[0].z0
+        self.r0 = self.objList[0].r0
+        self.z0 = self.objList[0].z0
 
-        for stg in self.stgList:
-            stg.translate([0.0, 0.0, self.z1])
-            self.z1 = self.z1 + stg.z1
+        for obj in self.objList:
+            obj.translate([0.0, 0.0, self.z1])
+            self.z1 = self.z1 + obj.z1
 
-        self.r1 = self.stgList[-1].r1
-
-    def translate(self, delta):
-
-        for stg in self.stgList:
-            stg.translate(delta)
-
-    def rotateZ(self, theta):
-
-        for stg in self.stgList:
-            stg.rotateZ(theta)
-
-    def plot(self, ax):
-
-        for stg in self.stgList:
-            stg.plot(ax)
-
-        return ax
+        self.r1 = self.objList[-1].r1
 
 
-class stage():
-
-    def __init__(self, modList):
-
-        self.modList = []
-        for mod in modList:
-            self.modList.append(mod.copy())
-
-        self.r0 = 0.0
-        self.r1 = 0.0
-        self.z0 = 0.0
-        self.z1 = 0.0
-
-        self.calculate()
-
-    def copy(self):
-
-        return stage(self.modList)
-
-    def calculate(self):
-        self.r0 = self.modList[0].r0
-        self.z0 = self.modList[0].z0
-
-        for mod in self.modList:
-            mod.translate([0.0, 0.0, self.z1])
-            self.z1 = self.z1 + mod.z1
-
-        self.r1 = self.modList[-1].r1
-
-    def translate(self, delta):
-
-        for mod in self.modList:
-            mod.translate(delta)
-
-    def rotateZ(self, theta):
-
-        for mod in self.modList:
-            mod.rotateZ(theta)
-
-    def plot(self, ax):
-
-        for mod in self.modList:
-            mod.plot(ax)
-
-        return ax
-
-
-class module():
+class module(group):
 
     def __init__(self, z, r, Nt, color):
 
@@ -161,11 +165,19 @@ class module():
         self.Nt = Nt
         self.Nr = len(self.r)
 
+        self.objList = []
         self.calculate()
 
     def copy(self):
 
-        return self.__init__(self.z, self.r, self.Nt, self.color)
+        return module(self.objList)
+
+    def copyObj(self, aux):
+
+        for obj in self.objList:
+            aux.objList.append(obj.copy())
+
+        return aux
 
     def calculate(self):
 
@@ -188,57 +200,12 @@ class module():
         y = numpy.outer(numpy.sin(theta), rr)
         z = numpy.outer(ones, zz)
 
-        self.surf = surfClass(x, y, z, self.color)
-
-    def plot(self, ax):
-
-        ax = self.surf.plot(ax)
-        return ax
+        self.objList = [surfClass(x, y, z, self.color)]
 
     def plotPerfil(self):
 
         plt.plot(self.z, self.r)
         plt.show()
-
-    def translate(self, delta):
-
-        self.surf.translate(delta)
-
-    def rotateZ(self, theta):
-
-        self.surf.rotateZ(theta)
-
-
-class surfClass():
-
-    def __init__(self, x, y, z, color):
-
-        self.x = x
-        self.y = y
-        self.z = z
-        self.color = color
-
-    def plot(self, ax):
-
-        ax.plot_surface(self.x, self.y, self.z, color=self.color)
-
-        return ax
-
-    def translate(self, delta):
-
-        self.x = self.x + delta[0]
-        self.y = self.y + delta[1]
-        self.z = self.z + delta[2]
-
-    def rotateZ(self, theta):
-
-        x0 = self.x.copy()
-        y0 = self.y.copy()
-        self.x = x0*numpy.cos(theta) + y0*numpy.sin(theta)
-        self.y = y0*numpy.cos(theta) - x0*numpy.sin(theta)
-
-######################
-# Specialized modules
 
 
 class cyl(module):
@@ -249,8 +216,8 @@ class cyl(module):
         self.R = R
         self.Nt = Nt
         self.color = color
-        self.z = [0, L, L]
-        self.r = [R, R, 0]
+        self.z = [0, L]
+        self.r = [R, R]
 
         self.Nr = len(self.r)
         self.calculate()
@@ -269,8 +236,8 @@ class cone(module):
         self.R2 = R2
         self.Nt = Nt
         self.color = color
-        self.z = [0, L, L]
-        self.r = [R1, R2, 0]
+        self.z = [0, L]
+        self.r = [R1, R2]
 
         self.Nr = len(self.r)
         self.calculate()
@@ -323,33 +290,74 @@ class coife(module):
                 self.zc.append(self.rho*numpy.sin(ang) + self.L - self.rho)
 
 
+class nozzle(stack):
+
+    def __init__(self, L, R, Nt, color):
+
+        self.L = L
+        self.R = R
+        self.Nt = Nt
+        self.color = color
+
+        c2 = numpy.array([1, 1, 1])*0.0
+        cyl1 = cyl(L, R, Nt, color)
+        cone1 = cone(L, R*0.75, R*0.25, Nt, c2)
+        cone2 = cone(0, R*0.25, 0.0, Nt, c2)
+        cone2.translate([0.0, 0.0, L])
+        cone3 = cone(0, R*0.95, R*0.55, Nt, color)
+        cone3.translate([0.0, 0.0, L/2])
+
+        self.objList = [cyl1, cone1, cone2, cone3]
+
+        self.r0 = R
+        self.r1 = R
+        self.z0 = 0.0
+        self.z1 = L
+
+    def copy(self):
+
+        return nozzle(self.L, self.R, self.Nt, self.color)
+
+
 if __name__ == "__main__":
 
     # Example of use:
 
     # colors based on a grey scale.
-    c1 = numpy.array([1,1,1])
+    c1 = numpy.array([1.0, 1.0, 1.0])
 
-    cyl1 = cyl( 1, 1, 50, c1)
-    cyl2 = cyl(10, 1, 50, c1*0.8)
-    cyl3 = cyl( 4, 1, 50, c1*0.6)
-    cyl4 = cyl( 6, 1, 50, c1*0.4)
-    coife1 = coife(3, 1, 0.3, 50, c1*0.5)
+    cyl1 = cyl(1, 1, 50, c1)
+    cyl2 = cyl(6, 1, 50, c1*0.8)
+    cone1 = cone(1.5, 1.5, 1.0, 50, c1)
+    cyl3 = cyl(10, 1.5, 50, c1*0.8)
+    cyl4 = cyl(3, 1, 50, c1*0.8)
+    cyl5 = cyl(0.5, 1, 50, c1*0.7)
+    coife1 = coife(3, 1, 0.3, 50, c1*0.7)
+    nozzle1 = nozzle(2, 1.5, 50, c1*0.5)
+    nozzle2 = nozzle(1.5, 1, 50, c1*0.5)
 
-    stg1 = stage([cyl3, cyl2, cyl1])
-    stg2 = stage([cyl4, cyl2, cyl1])
-    stg3 = stage([cyl1, coife1])
+    stg1 = stack([nozzle1, cyl3, cone1])
+    stg2 = stack([nozzle2, cyl2, cyl1])
+    stg3 = stack([nozzle2, cyl4, cyl1])
+    stgCoife = stack([cyl5, coife1])
 
-    body1 = body([stg1, stg2, stg3])
-    body2 = body([stg2, stg3])
+    rocket1 = stack([stg1, stg2, stg3, stgCoife])
+    booster1 = stack([stg2, stgCoife])
+    rocket1.around(booster1, 4, 2.7)
 
-    rocket1 = rocket(body1, body2, 4, 2.5)
+    mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(400, 300))
+    mlab.clf()
+    mlab = rocket1.plot(mlab)
+    mlab.show()
 
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111, projection='3d')
-    ax2 = rocket1.plot(ax2)
-    ax2.set_xlim(-rocket1.Lmax/2, rocket1.Lmax/2)
-    ax2.set_ylim(-rocket1.Lmax/2, rocket1.Lmax/2)
-    ax2.set_zlim(0, rocket1.Lmax)
-
-    plt.show()
+#==============================================================================
+#     #  Case using matplotlib (image quality low)
+#     fig2 = plt.figure()
+#     ax2 = fig2.add_subplot(111, projection='3d')
+#     ax2 = rocket1.matPlot(ax2)
+#     Lmax = rocket1.z1
+#     ax2.set_xlim(-Lmax/2, Lmax/2)
+#     ax2.set_ylim(-Lmax/2, Lmax/2)
+#     ax2.set_zlim(0, Lmax)
+#     plt.show()
+#==============================================================================
