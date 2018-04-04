2410fcd0f69325716db0aa54ca9af61af418e438
araujolma <levimaiaaraujo@gmail.com>
2018-04-04 11:43:17 -0300

    More solvers and experiments
    Further experiments were conducted, with new solvers, etc. No
    success in reproducing the results of Miele's 1970 paper, of course.
    
    Talking with Souza, he suggested to include an error calculation step
    for all the ODE integrations. This will be done now.

--- utils.py
+++ utils.py
@@ -37,6 +37,10 @@ def ddt(vec,N):
     dvec[N-1] = (vec[N-1]-vec[N-2])/dt
     for k in range(1,N-1):
         dvec[k] = .5*(vec[k+1]-vec[k-1])/dt
+#    for k in range(N-1):
+#        dvec[k] = vec[k+1]-vec[k]
+#    dvec[N-1] = dvec[N-2]
+#    dvec *= (1.0/dt)
     return dvec
 
 def testAlgn(x,y):
@@ -49,7 +53,7 @@ def testAlgn(x,y):
 def getNowStr():
     """Returns a string with the current time, all non numeric characters
     switched to _'s. """
-    
+
     thisStr = str(datetime.datetime.now())
     thisStr = thisStr.replace(' ','_')
     thisStr = thisStr.replace('-','_')
@@ -60,10 +64,10 @@ def getNowStr():
 if __name__ == "__main__":
     print("In utils.py!")
     print("Testing testAlgn:")
-    
+
     x = numpy.array([1.0,2.0,3.0])
     y = 5.0*x
     print(testAlgn(x,y))
     x[0]=0.0
     print(testAlgn(x,y))
-    
+
