917d746e4e278d8edade04091124b0051461a893
Luiz Gustavo Muniz <nascimento.lgmn@gmail.com>
2018-04-13 10:56:58 -0300

    Added function "compWith" and compare in "plotTraj"
    Functions added only for probBrac and probCart.

--- interf.py
+++ interf.py
@@ -390,7 +390,7 @@ class ITman():
             sol.rest(parallelOpt=self.parallelOpt)
             contRest += 1
             sol.plotSol()
-            input("\nInterf: One more restoration complete.")
+            #input("\nOne more restoration complete.")
 
         sol.showHistP()
 
@@ -554,6 +554,7 @@ class ITman():
                 sol.plotTraj()
                 if altSol is not None:
                     sol.compWith(altSol,'Initial guess')
+                    sol.plotTraj(True,altSol,'Initial guess')
 
             if self.gradRestPausCond(sol):
                 print("\a")
