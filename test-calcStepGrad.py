80af781358da3cdf783ee5cfa303e512de3212d7
araujolma <levimaiaaraujo@gmail.com>
2021-01-28 10:16:34 -0300

    calcStepGrad tester OK with interpolation curves
    As stated in the previous commit, I implemented the manual input
    for points in J and P, so the sgraDummy object interpolates/ex-
    trapolates a J or P vs. step behavior.
    
    The next task is to make a parser that looks through a log file
    from a MSGRA run and retrieves the steps that were evaluated, with
    their corresponding values of P and J.

--- test-calcStepGrad.py
+++ test-calcStepGrad.py
@@ -10,8 +10,11 @@ import matplotlib.pyplot as plt
 import grad_sgra
 from interf import logger
 from tabulate import tabulate
+from scipy.interpolate import interp1d
 
-parsDefault = {'minVal': 1.,
+parsDefault = {'useObjPars': True,
+               'usePPars': True,
+               'minVal': 1.,
                'minPt': 2.,
                'sigma': 1.,
                'scale': 2.,
@@ -51,6 +54,22 @@ class SgraDummy:
         self.x = 0.
         self.pi = numpy.array([1.])
 
+        if self.pars['useObjPars']:
+            self.obj = None
+        else:
+            # set up the interpolation model
+            obj = interp1d(self.pars['step_J_P_array'][0,:],
+                           self.pars['step_J_P_array'][1,:])
+            self.obj = obj
+
+        if self.pars['usePPars']:
+            self.Pfit = None
+        else:
+            # set up the interpolation model
+            Pfit = interp1d(self.pars['step_J_P_array'][0,:],
+                           self.pars['step_J_P_array'][2,:])
+            self.Pfit = Pfit
+
         self.tol = {'P': pars['tolP']}
         self.constants, self.restrictions = {}, {}
         for key in ['GSS_PLimCte', 'GSS_stopStepLimTol', 'GSS_stopObjDerTol',
@@ -65,31 +84,110 @@ class SgraDummy:
             self.dbugOptGrad[key] = pars[key]
 
     def calcP(self, mustPlotPint=False):
-        k = (self.constants['GSS_PLimCte'] * self.tol['P'] - self.pars['P0'])/\
-            self.pars['limP_step']
-        P = self.pars['P0'] + k * self.x
+        """Calculate current P value."""
+        if self.pars['usePPars']:
+            k = (self.constants['GSS_PLimCte'] * self.tol['P'] - self.pars['P0'])/\
+                self.pars['limP_step']
+            P = self.pars['P0'] + k * self.x
+        else:
+            P = self.PfitSat(self.x)
+
         return P, P, 0.
 
+    def PfitSat(self, x):
+        """A saturated version of P fit"""
+        xmin, xmax = min(self.Pfit.x), max(self.Pfit.x)
+        if x > xmax:
+            x = xmax
+        elif x < xmin:
+            x = xmin
+        return self.Pfit(x)
+
+    def getPLimStep(self, limPtoo=False):
+        """Get the value of alfa so that P(alfa)=P_lim."""
+        if self.pars['usePPars']:
+            if limPtoo:
+                limP = self.pars['tolP'] * self.pars['GSS_PLimCte']
+                return self.pars['limP_step'], limP
+            else:
+                return self.pars['limP_step']
+        else:
+            limP = self.pars['tolP'] * self.pars['GSS_PLimCte']
+
+            # basic bisection. Prepare upper and lower bounds
+            alfa_low = 0.
+            alfa_high = 1.
+            P = self.PfitSat(alfa_high)
+            while P < limP:
+                alfa_high *= 2.
+                P = self.PfitSat(alfa_high)
+            # perform bisection itself
+            alfa = .5 * (alfa_low + alfa_high)
+            P = self.PfitSat(alfa)
+            while abs(P-limP) > 1e-3 * limP:
+                if P > limP:
+                    alfa_high = alfa
+                else:
+                    alfa_low = alfa
+                alfa = .5 * (alfa_low + alfa_high)
+                P = self.PfitSat(alfa)
+
+            if limPtoo:
+                return alfa, limP
+            else:
+                return alfa
+
     def calcJ(self):
-        I = miele_curve(self.x, self.pars)
+        """Calculate current J value."""
+        if self.pars['useObjPars']:
+            I = miele_curve(self.x, self.pars)
+        else:
+            x, xmin, xmax = self.x, min(self.obj.x), max(self.obj.x)
+
+            if x > xmax:
+                x = xmax
+            elif x < xmin:
+                x = xmin
+
+            I = self.obj(x)
         #J, J_Lint, J_Lpsi, I, Iorig, Ipf
         return I, 0., 0., I, I, 0.
 
+    def getMinJStep(self, minJtoo=False):
+        """Get the value of alfa that minimizes objective J."""
+        if self.pars['useObjPars']:
+            if minJtoo:
+                return self.pars['minPt'], self.pars['minVal']
+            else:
+                return self.pars['minPt']
+        else:
+            arr = self.pars['step_J_P_array']
+            argMinPt = arr[1, :].argmin()
+            if minJtoo:
+                return arr[0, argMinPt], arr[1, argMinPt]
+            else:
+                return arr[0, argMinPt]
+
     def copy(self):
-        copia = SgraDummy(self.corr)
-        copia.x = self.x.copy()
-        copia.pars = self.pars.copy()
+        """Make a copy of itself."""
+        copia = SgraDummy(self.corr, pars=self.pars.copy())
+        copia.x = self.x + 0.
         return copia
 
-    def aplyCorr(self, alfa, correc):
-        self.x += alfa * correc['x']
-        self.pi += alfa * correc['pi']
+    def aplyCorr(self, alfa, correction):
+        """Apply the correction."""
+        self.x += alfa * correction['x']
+        self.pi += alfa * correction['pi']
 
     def calcStepGrad(self, *args, **kwargs):
         return grad_sgra.calcStepGrad(self, *args, **kwargs)
 
-    def showPJ(self, alfaMin=0., alfaMax=5.):
+    def showPJ(self, alfaMin=0., alfaMax=5., autoscale=True):
         """Make plots of the P and J curves as functions of alfa."""
+
+        if autoscale and 'step_J_P_array' in self.pars.keys():
+            alfaMin = min(self.pars['step_J_P_array'][0, :])*.95
+            alfaMax = max(self.pars['step_J_P_array'][0, :])*1.05
         alfa = numpy.linspace(alfaMin, alfaMax, num=1000)
         P = numpy.empty_like(alfa)
         J = numpy.empty_like(alfa)
@@ -107,6 +205,8 @@ class SgraDummy:
         ax1.plot(alfa, self.tol['P'] + numpy.zeros_like(P), '--', label='tolP')
         limP = self.constants['GSS_PLimCte'] * self.tol['P']
         ax1.plot(alfa, limP + numpy.zeros_like(P), '--', label='limP')
+        Ppt = self.getPLimStep()
+        ax1.plot(Ppt, limP, 'o', label='limP_pt')
         ax1.legend()
         ax1.grid()
         ax1.set_xlabel('Step')
@@ -114,7 +214,8 @@ class SgraDummy:
         ax1.set_title('P and J vs. step')
 
         ax2.plot(alfa, J, label='J')
-        ax2.plot(self.pars['minPt'], self.pars['minVal'], 'o', label='min_pt')
+        minPt, minVal = self.getMinJStep(minJtoo=True)
+        ax2.plot(minPt, minVal, 'o', label='min_pt')
         ax2.grid()
         ax2.set_xlabel('Step')
         ax2.set_ylabel('J')
@@ -143,18 +244,18 @@ class TestSeq:
             self.stepManList.append(stepMan)
             self.stepList.append(alfa)
             self.stopMotv.append(stepMan.stopMotv)
-            self.minObjPt.append(sgra.pars['minPt'])
-            self.limPPt.append(sgra.pars['limP_step'])
+            self.minObjPt.append(sgra.getMinJStep())
+            self.limPPt.append(sgra.getPLimStep())
             alfa_0 = alfa
 
     def checkPerf(self):
         """Check performance of calcStepGrad on the runs"""
         nTest = 0
         for alfa, stepMan, sgra in zip(self.stepList, self.stepManList, self.sgraList):
-            if sgra.pars['minPt'] < sgra.pars['limP_step']:
-                alfa_exact = sgra.pars['minPt']
-            else:
-                alfa_exact = sgra.pars['limP_step']
+            alfa_J_min = sgra.getMinJStep()
+            alfa_P = sgra.getPLimStep()
+            alfa_exact = min(alfa_J_min, alfa_P)
+
             self.stepError.append(alfa - alfa_exact)
             self.stepRelError.append((alfa - alfa_exact)/alfa_exact)
             self.nAttempts.append(stepMan.cont)
@@ -177,30 +278,31 @@ class TestSeq:
         print(tabulate(data, headers=headers, floatfmt='.3G'))
 
 if __name__ == "__main__":
-    # corr = {'x': 1.,
-    #         'pi': numpy.array([1e-10]),
-    #         'dJdStepTheo': -1.}
-    # thesePars = parsDefault
-    # thesePars['limP_step'] = 1.5
-    # sgra = SgraDummy(corr, pars=thesePars)
-    #
-    # sgra.showPJ()
-    #
-    # alfa_0 = 0.
-    # retry_grad = False
-    # stepMan = None
-    # alfa, stepMan = sgra.calcStepGrad(sgra.corr, alfa_0, retry_grad, stepMan)
 
     thesePars = parsDefault
     thesePars['limP_step'] = 1.5
     thesePars['plotCalcStepGrad'] = False
     parsList = [thesePars]
+
     newPars = thesePars.copy()
     newPars['limP_step'] = 10.
     parsList.append(newPars)
+
     newPars = newPars.copy()
     newPars['minPt'] = 1e-4
     parsList.append(newPars)
+
+    parsNonPar = parsDefault.copy()
+    parsNonPar['limP_step'] = 4.
+    parsNonPar['useObjPars'] = False # this makes it interpolate for calculating J
+    parsNonPar['usePPars'] = False # this makes it interpolate for calculating P
+    sJP = numpy.empty((3, 11))
+    sJP[0,:] = numpy.linspace(0.,10.,num=11)
+    sJP[1,:] = numpy.array([1., 1., 0.7, 0.5, -.25, -1., -.6, .5, .5, .7, 1.]) + 2.
+    sJP[2,:] = parsNonPar['P0'] + 1.1e-5 * sJP[0,:]
+    parsNonPar['step_J_P_array'] = sJP
+    parsList.append(parsNonPar)
+
     TS = TestSeq(parsList, showPJ=True)
     TS.checkPerf()
 
